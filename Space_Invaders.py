6a643d01936e4f99c492a40731daf540c57f4cec
Glenn Kinstler <glenn@glennkinstler.com>
2018-10-18 10:42:53 -0500

    added aliens

--- Space_Invaders.py
+++ Space_Invaders.py
@@ -24,6 +24,7 @@ clock = pygame.time.Clock()
 
 playerImg = pygame.image.load("si-player.gif")
 backgroundImg = pygame.image.load("si-background.gif")
+enemyImg = pygame.image.load('si-enemy.gif')
 
 class Player:
     xcor = 60
@@ -47,7 +48,26 @@ class Player:
     def stopMoving(self):
         self.direction = 0
 
+class Enemy:
+    xcor = 0
+    ycor = 0
+    speed = 1
+    direction = 1
+    def show(self):
+        self.xcor += self.direction * self.speed
+        gameDisplay.blit(enemyImg, (self.xcor, self.ycor))
+    def createEnemies():
+        newEnemies = []
+        for x in range(0, 5):
+            for y in range(0, 3):
+                newEnemy = Enemy()
+                newEnemy.xcor = gameSideMargin + gameBorderWidth + enemyImg.get_width() * x
+                newEnemy.ycor = gameTopMargin + gameBorderWidth + enemyImg.get_height() * y
+                newEnemies.append(newEnemy)
+        return newEnemies
+
 player = Player()
+enemies = Enemy.createEnemies()
 
 isAlive = True
 while isAlive:
@@ -75,6 +95,9 @@ while isAlive:
 
     pygame.draw.rect(gameDisplay, white, (gameSideMargin, gameTopMargin, windowWidth - gameSideMargin * 2, windowHeight - gameBottomMargin - gameTopMargin))
     gameDisplay.blit(backgroundImg, (gameSideMargin + gameBorderWidth, gameTopMargin + gameBorderWidth), (0, 0, gameWidth, gameHeight))
+    
+    for enemy in enemies:
+        enemy.show()
     
     player.show()
     
